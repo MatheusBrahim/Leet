2a2b9078302aa4b03969372db82bcb728ee1c359
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:15:30 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string paraCriptografar;
+string criptografado;
+
+Console.Clear();
+
+Console.WriteLine("Qual frase você quer Criptografar?");
+paraCriptografar = Console.ReadLine().ToUpper();
+
+criptografado = paraCriptografar
+            .Replace("A", "4")
+            .Replace("E", "3")
+            .Replace("I", "1")
+            .Replace("O", "0")
+            .Replace("T", "7")
+            .Replace("S", "5");
+
+Console.WriteLine(criptografado);
